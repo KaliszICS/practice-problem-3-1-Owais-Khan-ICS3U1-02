420584882d711ba86dff31c293fc08a567db2664
Owais-Khan-ICS3U1-02 <343318663@tdsb.ca>
2026-03-24 10:34:58 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,43 @@
+/**
+ * File: If statements
+ * Author: Owais Ali Khan
+ * Date Created: Mar 24, 2026
+ * Date Last Modified: Mar 24, 2026
+ */
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
+
+	public static String evenOrOdd(int num) {
+        if (num%2==0) {
+            return "Even";
+        }
+        else {
+            return "Odd";
+        }
+    }
+    
+    public static String teacherOrStudent(String name) {
+        if (name.equals("Kalisz")) {
+            return "Teacher";
+        }
+        else {
+            return "Student";
+        }
+    }
+    
+    public static String fartherFromZero(int num) {
+        if (num > 0) {
+            return num+5;
+        } else if (num == 0) {
+            return 0;
+        } else {
+            return num-5;
+        }
+        
+    }
 
 }
